7ccbc81fb81eb1297672f3e740fb31770ac1b5e2
rustamkremcheev <iloveyouaselya1996>
2022-04-14 22:45:05 +0500

    Commit 14.04.2022

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+//BigInt bNum = 102323345324234;
+//console.log(bNum);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 50,
+    isMarried: false
+};
+
+//obj.a = 30;
+
+console.log(obj);
+
+
+console.log(name);
+var name = 'Ivan';
+
+
+const bool = true;
+console.log(bool);
+
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`Hello, ${user}`);
