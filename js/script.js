d77a4f3e51a31c6b422d1d8d75d6756b4e590399
rustamkremcheev <rustam.omg1996@gmail.com>
2022-04-15 11:31:46 +0500

    changes script js

--- js/script.js
+++ js/script.js
@@ -28,10 +28,30 @@ var name = 'Ivan';
 const bool = true;
 console.log(bool);
 
-
 const category = 'toys';
 
 console.log(`https://someurl.com/${category}/5`);
 
 const user = 'Ivan';
 alert(`Hello, ${user}`);
+
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: null,
+    genres: [],
+    privat: false
+};
+
+let film = prompt('Один из последних просмотренных фильмов?');
+let score = prompt('На сколько оцените его?');
+
+personalMovieDB.movies = {
+    [film]: score
+};
+
+console.log(personalMovieDB.movies);
