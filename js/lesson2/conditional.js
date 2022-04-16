0fb6ef6a404dd496381c39231d2c1ea43b2d6d7a
rustamkremcheev <rustam.omg1996@gmail.com>
2022-04-16 20:25:23 +0500

    add new folder and file

---
+++ js/lesson2/conditional.js
@@ -0,0 +1,32 @@
+"use strict";
+
+if(4 == 4) {
+   console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49){
+   console.log('Error');
+} else if (num > 100) {
+   console.log('Много!');
+}
+
+(num == 50) ? console.log('Ok!') : console.log('Error!');
+
+switch (num) {
+   case 49: 
+        console.log('Неверно');
+        break;
+   case 100:
+        console.log('Неверно');
+        break;
+   case 50:
+        console.log('В Точку!');
+        break;
+   default:
+        console.log('Не в этот раз');
+        break;
+}
