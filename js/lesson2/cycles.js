cadf5efad21063973c52d8bace2e77cef69137d9
rustamkremcheev <rustam.omg1996@gmail.com>
2022-04-16 20:38:16 +0500

    finished cycles and cndition lessons

--- js/lesson2/cycles.js
+++ js/lesson2/cycles.js
@@ -1,1 +1,24 @@
-const num = 50;
+let num = 50;
+
+/*while (num <= 55) {
+    console.log(num);
+    num++;
+} 
+
+
+do {
+    console.log(num);
+    num++;
+} while(num < 55); */
+
+
+for (let i = 0; i < 10; i++) {
+
+    if (i === 6) {
+        //break;
+        continue;
+    }
+
+    console.log(i);
+    
+}
