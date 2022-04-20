5cf1b1fd980ba87fbf2729c2622acc1d2ecf5319
rustamkremcheev <rustam.omg1996@gmail.com>
2022-04-20 14:32:27 +0500

    Callback Lesson

---
+++ js/lesson2/callback-lesson.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function first() {
+    
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`);
+    callback();
+}
+
+
+function done() {
+    console.log('I finished this lesson!');
+}
+
+learnJS('JavaScript', done);
+
+
